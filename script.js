a5e9fc88948bea988e2d85d1f302f8e11a227be0
DARSHAN SANJAY BHATARKAR <darshanbhatarkar04@gmail.com>
2024-07-29 11:02:01 +0530

    git commit git push origin master

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+
+
+var typed = new Typed(".learning",{
+    strings : ["Development","Programming", "Designing"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
+
+</script>
